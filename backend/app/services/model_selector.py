61b6267c89f21f55be890bb812787a81185a1cf1
Cikguseven <kieronsevenlee@gmail.com>
2024-10-29 16:44:16 +0800

    Add Random Forest model

--- backend/app/services/model_selector.py
+++ backend/app/services/model_selector.py
@@ -1,17 +1,23 @@
 from app.models import PredictionRequest
 from app.services.knn_predict import knn_predict
 from app.services.knn_predict import knn_pipeline
+from app.services.rf_predict import rf_predict
+from app.services.rf_predict import rf_pipeline
 from app.services.lime_pipeline import explain_prediction
 
 def select_model_predict(model_name: str):
     if model_name == "knn":
         return knn_predict
+    elif model_name == "rf":
+        return rf_predict
     else:
         return None
 
 def select_model_pipeline(model_name: str):
     if model_name == "knn":
         return knn_pipeline
+    elif model_name == "rf":
+        return rf_pipeline
     else:
         return None
 
@@ -22,7 +28,7 @@ def predict_for_model(model_name: str, data: PredictionRequest):
         return prediction[0]
     else:
         return None
-    
+
 def explain_for_model(model_name: str, data: PredictionRequest):
     model_selected = select_model_pipeline(model_name)
     print(explain_prediction(data.convert_to_pandas_df(), model_selected))
